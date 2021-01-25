306d8a15d260c48926c29e2fee351e9a6b28cf6e
Matúš Klasovitý <matusklaso@gmail.com>
2021-01-25 18:47:54 +0100

    NavBar komponent

--- zadanie/tailwind.config.js
+++ zadanie/tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#CD8A64",
+        labelTextColor: "#9F9F9F",
+        headlineColor: "#000000",
+        subHeadlineColor: "#2F2F2F",
+        borderColor: "#DFDFDF",
+      },
+    },
   },
   variants: {
     extend: {},
